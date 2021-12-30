458bed287e1b2d6f2b309d8f7141f5e57a2c583e
mtraiano76 <mtraiano76@gmail.com>
2021-12-30 07:56:55 -0300

    Html

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 
 app.listen(3030,() =>
-console.log ('DH-Heroes')
-);
+console.log ('DH-Heroes'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/babbage.html' ))
+});
